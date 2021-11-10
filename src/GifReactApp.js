8516611f01774683ed83781b93015f938e392241
Victor Rodriguez <rodriguezvictor.root@gmail.com>
2021-11-09 22:01:31 -0600

    adding custom hook + animations

---
+++ src/GifReactApp.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifReactApp = () => {
+
+    const [categories, setCategories] = useState(['jojos']);
+
+    // const handleAdd = () => {
+    //     setCategories([...categories, 'YuYu Hakusho']);
+    // }
+
+    return (
+        <>
+            <h2>GifReactApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+            <hr/>
+
+            <ol>
+                {
+                    categories.map(category => 
+                        <GifGrid 
+                            key={ category }
+                            category={ category }
+                        />
+                    )
+                }
+            </ol>
+        </>
+    )
+}
